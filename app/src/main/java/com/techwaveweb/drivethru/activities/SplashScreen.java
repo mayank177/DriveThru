ffab4ef30359604dc4836c8cf8834b775174b764
mayank177 <jainmayank.662@gmail.com>
2020-09-28 12:17:22 +0530

    initial commit

---
+++ app/src/main/java/com/techwaveweb/drivethru/activities/SplashScreen.java
@@ -0,0 +1,19 @@
+package com.techwaveweb.drivethru.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import com.techwaveweb.drivethru.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
