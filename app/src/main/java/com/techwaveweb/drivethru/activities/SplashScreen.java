c2967fbf5e91bbc1bac4a0309a6286a0d39ef196
mayank177 <jainmayank.662@gmail.com>
2020-09-28 18:46:55 +0530

    initial commit

--- app/src/main/java/com/techwaveweb/drivethru/activities/SplashScreen.java
+++ app/src/main/java/com/techwaveweb/drivethru/activities/SplashScreen.java
@@ -2,12 +2,17 @@ package com.techwaveweb.drivethru.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.WindowManager;
 
+import com.techwaveweb.drivethru.MainActivity;
 import com.techwaveweb.drivethru.R;
 
 public class SplashScreen extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +20,14 @@ public class SplashScreen extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashScreen.this, WelcomeActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
     }
 }
