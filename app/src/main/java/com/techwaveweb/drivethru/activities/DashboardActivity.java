1d108aed7680ff531503b500bd7c2eefbfd037d5
mayank177 <jainmayank.662@gmail.com>
2020-10-06 13:00:28 +0530

    add some screens and activity

---
+++ app/src/main/java/com/techwaveweb/drivethru/activities/DashboardActivity.java
@@ -0,0 +1,16 @@
+package com.techwaveweb.drivethru.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.techwaveweb.drivethru.R;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
